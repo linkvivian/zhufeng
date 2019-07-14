cbfd898e8550cd2f38d82de81059e0ea57f315d4
fandonglai <704072802@qq.com>
2019-07-14 15:08:07 +0800

    promise-then

--- 2.promise/promise.js
+++ 2.promise/promise.js
@@ -1,48 +1,101 @@
-const SUCCESS = 'fulfilled'
-const FAIL = 'rejected';
-const PENDING = 'pending'
+const PENDING = "PENDING";
+const SUCCESS = "FULFILLED";
+const FAIL = "REJECTED";
+// 返还的那个新的promise x 是then方法中的返回值 
+function resolvePromise(promise2, x,resolve,reject) { // 考虑的非常全面
+    if(promise2 === x){
+       return reject(new TypeError('TypeError: Chaining cycle detected for promise #<Promise>'));
+    }
+    // 判断x的类型
+}
 class Promise {
   constructor(executor) {
-    this.status = PENDING; // 默认是等待态
-    this.value= undefined;
+    this.status = PENDING;
+    this.value = undefined;
     this.reason = undefined;
-    this.onResolvedCallbacks = []; // 存储成功的所有的回调 只有pending的时候才存储
-    this.onRejectedCallbacks = []; // 存储所有失败的
-    let resolve = (value) => { // 成功
-        if(this.status === PENDING){
-            this.value = value;
-            this.status = SUCCESS;
-            this.onResolvedCallbacks.forEach(fn=>fn());
-        }
+    this.onResolvedCallbacks = [];
+    this.onRejectedCallbacks = [];
+    const resolve = value => {
+      if (this.status === PENDING) {
+        this.value = value;
+        this.status = SUCCESS;
+        this.onResolvedCallbacks.forEach(fn => fn());
+      }
     };
-    let reject = (reason) => { // 失败
-        if(this.status === PENDING){ 
-            this.reason = reason;
-            this.status = FAIL;
-            this.onRejectedCallbacks.forEach(fn=>fn());
-        }
+    const reject = reason => {
+      if (this.status === PENDING) {
+        this.reason = reason;
+        this.status = FAIL;
+        this.onRejectedCallbacks.forEach(fn => fn());
+      }
     };
-    try{
-        executor(resolve,reject);
-    }catch(e){
-        reject(e);
+    try {
+      executor(resolve, reject);
+    } catch (e) {
+      console.log(e);
+
+      reject(e);
     }
   }
-  then(onFulfilled,onRejected){ // 默认看一下状态调用对应的函数
-    if(this.status === SUCCESS){
-        onFulfilled(this.value);
-    }
-    if(this.status === FAIL){
-        onRejected(this.reason);
-    }
-    if(this.status === PENDING){
-        this.onResolvedCallbacks.push(()=>{
-            onFulfilled(this.value);
+  // 同一个promise then 多次
+  then(onFulfilled, onRejected) {
+    let promise2;
+    // 可以不停的调用then方法,返还了一个新的promise
+    promise2 = new Promise((resolve, reject) => {
+      if (this.status === SUCCESS) {
+        setTimeout(() => {
+          try {
+            // 调用当前then方法的结果，来判断当前这个promise2 是成功还是失败
+            let x = onFulfilled(this.value);
+            // 这里的x是普通值还是promise
+            // 如果是一个promise呢？
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (err) {
+            console.log(err);
+
+            reject(err);
+          }
         });
-        this.onRejectedCallbacks.push(()=>{
-            onRejected(this.reason);
-        })
-    }
+      }
+      if (this.status === FAIL) {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (err) {
+            console.log(err);
+
+            reject(err);
+          }
+        });
+      }
+      if (this.status === PENDING) {
+        this.onResolvedCallbacks.push(()=>{
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+              console.log(err);
+              reject(err);
+            }
+          });
+        });
+        this.onRejectedCallbacks.push(()=> {
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+                console.log(err);
+              reject(err);
+            }
+          });
+        });
+      }
+    });
+    return promise2;
   }
 }
+
 module.exports = Promise;
