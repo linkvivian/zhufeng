b6941cbaa4b11852ee190deeab8a4fa5813bde95
fandonglai <704072802@qq.com>
2019-07-21 12:03:14 +0800

    ejs

--- 4.es6/1.arr.js
+++ 4.es6/1.arr.js
@@ -74,14 +74,51 @@ function addTag(str) {
 //         }
 //     })
 // }
-let compose = (...fns)=> fns.reduce((a,b)=>(...args)=> a(b(...args)))
-let r = compose(
-  addTag,
-  len,
-  sum
-)("a", "b", "c");
-// reduce 方法 必须函数不能为空
-let newr = [1].reduce((a,b)=>{
-    console.log(a,b);
-})
-console.log(newr);
+// let compose = (...fns)=> fns.reduce((a,b)=>(...args)=> a(b(...args)))
+// let r = compose(
+//   addTag,
+//   len,
+//   sum
+// )("a", "b", "c");
+// // reduce 方法 必须函数不能为空
+// let newr = [1].reduce((a,b)=>{
+//     console.log(a,b);
+// })
+// console.log(newr);
+
+
+Array.prototype.reduce = function(callback,prev){
+    for(let i = 0; i < this.length;i++){
+        if(typeof prev === 'undefined'){
+            prev = callback(this[i],this[i+1],i+1,this);
+            i++;
+        }else{
+            prev = callback(prev,this[i],i,this);
+        }
+    }
+    return prev;
+}
+let r = [1,2,3,4].reduce((a,b,index,arr)=>{
+    return a+b;
+},5)
+console.log(r);
+// 返回一个映射后的数组
+let newArr = [1,2,3].map(item=>item*=2);
+console.log(newArr)
+// 返回一个过滤后的数组
+let filter = [1,2,3].filter(item=>item == 2);
+console.log(filter);
+// 找到true 就返回true
+let some = [1,2222,3].some(item=>{
+    return /2/.test(item);
+});
+console.log(some)
+// 找到false 就返回false
+let every = [1,2,3].every(item=>item==2);
+console.log(every)
+// 找到后返回找到的那一项
+let find = [1,2,3].find(item=>item==2);
+console.log(find);
+// findIndex
+// 数组 includes
+console.log([1,2,3].includes(3))
